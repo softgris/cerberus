a891173a1d7d19f56cc38e47326980b8b0868e78
Marius Grigore <mariusgrygore@gmail.com>
2021-08-26 18:20:56 +0100

    Initial commit

---
+++ src/main/java/com/softgris/cerberus/CerberusApplication.java
@@ -0,0 +1,13 @@
+package com.softgris.cerberus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerberusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerberusApplication.class, args);
+	}
+
+}
